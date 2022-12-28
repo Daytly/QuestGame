63fd548f8ada659f9687193cf53921b260ca76b5
Daytly <115785845+Daytly@users.noreply.github.com>
2022-12-28 17:00:12 +0700

    Button

--- Button.py
+++ Button.py
@@ -1,17 +1,20 @@
+import pygame
+
+
 class Button:
-    def __init__(self, width,height):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
 
-    def draw(self, x, y, massage, color1, color2, color3,color4=20,color5=20,color6=200, dis=10, action=None):
+    def draw(self, x, y, massage, color1, color2, color3, color4=20, color5=20, color6=200, dis=10, action=None):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         if x < mouse[0] < x + self.width:
             if y < mouse[1] < y + self.height:
                 pygame.draw.rect(display, (color1, color2, color3), (x, y, self.width, self.height))
                 if click[0] == 1:
-                    pygame.mixer.Sound.play(mp3_button)
-                    pygame.time.delay(170)
+                    # pygame.mixer.Sound.play(mp3_button)
+                    # pygame.time.delay(170)
                     if action is not None:
                         action()
             else:
