24dc6195d5dd6cc0eee0acb169bb25e367cf00c9
Daytly <115785845+Daytly@users.noreply.github.com>
2022-12-28 14:12:13 +0700

    Tile

---
+++ Tile.py
@@ -0,0 +1,6 @@
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
