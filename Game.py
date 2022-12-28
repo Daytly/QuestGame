910649e704b8a48ecfa52fcadb035b597d6122b1
Daytly <115785845+Daytly@users.noreply.github.com>
2022-12-28 14:15:08 +0700

    Game

--- Game.py
+++ Game.py
@@ -3,6 +3,8 @@ import sys
 import pygame
 from Ufo import Ufo
 
+from Camera import Camera
+
 
 class Game:
     def __init__(self):
@@ -11,15 +13,28 @@ class Game:
         self.screen = self.display.set_mode((700, 700))
         self.clock = pygame.time.Clock()
         self.fps = 60
+        self.player = None
+        self.camera = Camera()
+        # группы спрайтов
+        self.all_sprites = pygame.sprite.Group()
+        self.tiles_group = pygame.sprite.Group()
+        self.player_group = pygame.sprite.Group()
 
     def run(self):
         while True:
             self.screen.fill(pygame.Color('white'))
+            self.clock.tick(self.fps)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    self.all_sprites.update(event)
+            # изменяем ракурс камеры
+            self.camera.update(player)
+            # обновляем положение всех спрайтов
+            for sprite in self.all_sprites:
+                self.camera.apply(sprite)
             self.display.flip()
-            self.clock.tick(self.fps)
 
 
 game = Game()
