304f63329f61e257953084eac918a5398bd9e142
Daytly <115785845+Daytly@users.noreply.github.com>
2023-01-05 14:42:23 +0700

    sprites

--- player.py
+++ player.py
@@ -16,22 +16,22 @@ class Player(DynamicGameObject):
                 if self.check(self.coord.x, self.coord.y - 1):
                     self.rect.y -= 50
                     self.coord.y -= 1
-                self.cur_frame = 1
+                self.update_sprite(1)
             if args[0].key == pygame.K_DOWN:
                 if self.check(self.coord.x, self.coord.y + 1):
                     self.rect.y += 50
                     self.coord.y += 1
-                self.cur_frame = 0
+                self.update_sprite(0)
             if args[0].key == pygame.K_RIGHT:
                 if self.check(self.coord.x + 1, self.coord.y):
                     self.rect.x += 50
                     self.coord.x += 1
-                self.cur_frame = 3
+                self.update_sprite(3)
             if args[0].key == pygame.K_LEFT:
                 if self.check(self.coord.x - 1, self.coord.y):
                     self.rect.x -= 50
                     self.coord.x -= 1
-                self.cur_frame = 2
+                self.update_sprite(2)
             self.image = self.frames[self.cur_frame]
 
     def check(self, x, y):
--- wall.py
+++ wall.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from player import Player
 from staticGameObject import StaticGameObject
@@ -6,7 +8,7 @@ from staticGameObject import StaticGameObject
 class Wall(StaticGameObject):
     def __init__(self, sheet, pos_x, pos_y, game, cols, rows):
         super().__init__(sheet, pos_x, pos_y, game, cols, rows)
-
+        self.update_sprite(random.randrange(cols))
         self.solid = False
 
 
