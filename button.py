0b88ee0ac7663d14e4ef757f8dba011971bc0920
Daytly <115785845+Daytly@users.noreply.github.com>
2023-01-08 12:24:26 +0700

    buttons

--- button.py
+++ button.py
@@ -3,24 +3,26 @@ from functions import print_text
 
 
 class Button:
-    def __init__(self, width, height, x, y, massage, staticColor, activeColor, dis=10, action=None):
+    def __init__(self, width, height, x, y, massage, staticColor, activeColor, dis=10, action=None, image='Data'
+                                                                                                          '/sprites'
+                                                                                                          '/none.png'):
         self.width = width
         self.height = height
-        self.x = x
-        self.y = y
         self.massage = massage
         self.staticColor = staticColor
         self.activeColor = activeColor
         self.dis = dis
         self.action = action
+        self.image = pygame.transform.scale(pygame.image.load(image), (width, height))
+        self.rect = self.image.get_rect().move(x, y)
         self.isPressed = False
 
     def draw(self, screen):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-        if self.x < mouse[0] < self.x + self.width:
-            if self.y < mouse[1] < self.y + self.height:
-                pygame.draw.rect(screen, self.staticColor, (self.x, self.y, self.width, self.height))
+        if self.rect.x < mouse[0] < self.rect.x + self.width:
+            if self.rect.y < mouse[1] < self.rect.y + self.height:
+                pygame.draw.rect(screen, self.staticColor, (self.rect.x, self.rect.y, self.width, self.height))
                 if click[0] == 1:
                     # pygame.mixer.Sound.play(mp3_button)
                     # pygame.time.delay(170)
@@ -30,10 +32,10 @@ class Button:
                 else:
                     self.isPressed = False
             else:
-                pygame.draw.rect(screen, self.activeColor, (self.x, self.y, self.width, self.height))
+                pygame.draw.rect(screen, self.activeColor, (self.rect.x, self.rect.y, self.width, self.height))
         else:
-            pygame.draw.rect(screen, self.activeColor, (self.x, self.y, self.width, self.height))
-        print_text(screen, self.x + (self.width - len(self.massage) * 20) // 2, self.y + (self.height - 20) // 2,
+            pygame.draw.rect(screen, self.activeColor, (self.rect.x, self.rect.y, self.width, self.height))
+        print_text(screen, self.rect.x + (self.width - len(self.massage) * 20)//2, self.rect.y + (self.height - 20)//2,
                    30, self.massage, (200, 0, 0))
 
 
@@ -48,17 +50,21 @@ class ButtonLevel(Button):
     def draw(self, screen):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-        if self.x < mouse[0] < self.x + self.width:
-            if self.y < mouse[1] < self.y + self.height:
-                pygame.draw.rect(screen, self.staticColor, (self.x, self.y, self.width, self.height))
+        if self.rect.x < mouse[0] < self.rect.x + self.width:
+            if self.rect.y < mouse[1] < self.rect.y + self.height:
+                pygame.draw.rect(screen, self.staticColor, (self.rect.x, self.rect.y, self.width, self.height))
                 if click[0] == 1:
                     # pygame.mixer.Sound.play(mp3_button)
                     # pygame.time.delay(170)
-                    if self.action is not None:
+                    if self.action is not None and not self.isPressed:
                         self.action(self.level)
+                        self.isPressed = True
+                else:
+                    self.isPressed = False
             else:
-                pygame.draw.rect(screen, self.activeColor, (self.x, self.y, self.width, self.height))
+                pygame.draw.rect(screen, self.activeColor, (self.rect.x, self.rect.y, self.width, self.height))
         else:
-            pygame.draw.rect(screen, self.activeColor, (self.x, self.y, self.width, self.height))
-        print_text(screen, self.x + (self.width - len(self.massage) * 20) // 2, self.y + (self.height - 20) // 2,
+            pygame.draw.rect(screen, self.activeColor, (self.rect.x, self.rect.y, self.width, self.height))
+        print_text(screen, self.rect.x + (self.width - len(self.massage) * 20) // 2, self.rect.y + (self.height - 20) // 2,
                    30, self.massage, (200, 0, 0))
+
